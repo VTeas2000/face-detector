ec2c29ebe109ddedc528860cfa57a4419fd1a917
VTeas2000 <vteas2000@gmail.com>
2022-11-30 17:46:34 -0800

    init

---
+++ src/components/FaceDetector/FaceDetector.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './FaceDetector.css';
+
+const FaceDetector = ({ imageUrl, box }) => {
+  return (
+    <div className='center ma'>
+      <div className='absolute mt2'>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+      </div>
+    </div>
+  );
+}
+
+export default FaceDetector;
